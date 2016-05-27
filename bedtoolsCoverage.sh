5c5f7ba65d8e6de7fd04a633a3c97a5e8496f59e
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-05-27 10:04:39 +0930

    Trouble shooting Output is missing from file, so the script has been modified to try and work out why the output cannot be found.

--- bedtoolsCoverage.sh
+++ bedtoolsCoverage.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-L1=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Data/L1Location
-ALIGN=/Users/brittanyhowell/Documents/University/Honours_2016/Project/Genomes/STAR/testOutput
+L1=/mnt/project/location
+ALIGN=/mnt/project/STAR/mouse/output
 
+echo "Commencing program" > record.txt
+date >> record.txt
 
+echo ${ALIGN} >> record.txt
+echo ${L1} >> record.txt
 
-bedtools coverage -hist -F 0.2 -s -split -a ${L1}/L1_Mouse_bothorf.bed -b ${ALIGN}/mm10Aligned.sortedByCoord.out.bam
-
+#bedtools coverage -a ${L1}/L1_Mouse_bothorf.bed -b ${ALIGN}/mm10Aligned.sortedByCoord.out.bam >> record.txt
+#-hist -F 0.2 -s -split 
+echo "Finishing at" >> record.txt 
+date >> record.txt 
 # -hist: makes histograms and additional data 
 # -d: records depth at each L1 position
 # -F: Minimum overlap with read (0.2 = ~20bp)
