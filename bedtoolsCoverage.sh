9a14ff967b6729ca9bc50230887a4cbc72a071dd
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-05-27 16:13:29 +0930

    Addition I added a removal of the coverage file if statement, and I removed the printing of the statements to "records.txt" and made it print to stdout instead.

--- bedtoolsCoverage.sh
+++ bedtoolsCoverage.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-echo "Commencing program" > record.txt
-date >> record.txt
+echo "Commencing program" 
+date
+
+if [ -f coverage.out ];
+	then rm coverage.out
+	fi  
 
 bedtools coverage -hist -F 0.2 -s -split  -a /mnt/project/STAR/mouse/output/mm10Aligned.sortedByCoord.out.bam  -b /mnt/project/location/L1_Mouse_bothorf.bed > coverage.out
 
-echo "Finishing at" >> record.txt 
-date >> record.txt 
+echo "Finishing at" 
+date 
 # -hist: makes histograms and additional data 
 # -d: records depth at each L1 position
 # -F: Minimum overlap with read (0.2 = ~20bp)
